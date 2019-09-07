6f4df6459b0a506f366aaeb1c13bb337ee59bd2c
Eddy Almanza <a01568010@gmail.com>
2019-09-06 20:27:28 -0600

    Added final scores

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func pickWinner(lines []string) {
+
+	//We split the strings
+	onlyString := strings.Join(lines, " ")
+	stringSlice := strings.Split(onlyString, " ")
+
+	var intarray = []int{}
+
+	i := 1
+
+	for i <= len(stringSlice) {
+		//we convert the string arrays to int arrays
+		j, err := strconv.Atoi(stringSlice[i])
+		if err != nil {
+			panic(err)
+		}
+		intarray = append(intarray, j)
+
+		//we increment by 2 because we want to ignore the names
+		i = i + 2
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(intarray)))
+
+	k := 0
+
+	fmt.Print("The highest scores are: ")
+
+	for k < 3 {
+
+		fmt.Println(intarray[k])
+		k++
+	}
+
+}
+
+func openFile(text string) {
+
+	file, err := os.Open(text)
+
+	if err != nil {
+		log.Fatalf("Failed opening the file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+
+	pickWinner(txtlines)
+}
+
+func main() {
+
+	openFile("result.txt")
+
+}
